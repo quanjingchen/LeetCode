7c0905e005a081d8c408cd2cb560762622f2f606
quanjingchen <quanjingchen@gmail.com>
2023-03-29 21:28:03 -0700

    Time: 21 ms (88.23%), Space: 13.7 MB (71.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,5 +1,31 @@
 class Solution(object):
-
+    @staticmethod
+    def find_min(nums):
+        low = 0
+        high = len(nums) - 1
+        if nums[low] < nums[high]:
+            return low
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] < nums[high]:
+                high = mid
+            elif nums[mid] > nums[high]:
+                low = mid + 1 
+        return low
+    
+    @staticmethod
+    def binarySearch(nums, target):
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1 
+            else:
+                return mid
+        return -1
     
     def search(self, nums, target):
         """
@@ -7,44 +33,13 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        def find_min(nums):
-            low = 0
-            high = len(nums) - 1
-            if nums[low] < nums[high]:
-                return low
-            while low < high:
-                mid = (low + high) // 2
-                if nums[mid] < nums[high]:
-                    high = mid
-                elif nums[mid] > nums[high]:
-                    low = mid + 1 
-            return low
-        
-                
-        def binarySearch(nums, target):
-            low = 0
-            high = len(nums) - 1
-            while low <= high:
-                mid = (low + high) // 2
-                if nums[mid] < target:
-                    low = mid + 1
-                elif nums[mid] > target:
-                    high = mid - 1 
-                else:
-                    return mid
-            return -1
-        
-        min_index = find_min(nums)
+        min_index = Solution.find_min(nums)
 
         if min_index == 0:
-            return binarySearch(nums, target)
+            return Solution.binarySearch(nums, target)
         if target >= nums[0]:
-            return binarySearch(nums[:min_index], target)
-        ans = binarySearch(nums[min_index:], target)
+            return Solution.binarySearch(nums[:min_index], target)
+        ans = Solution.binarySearch(nums[min_index:], target)
         if ans == -1:
             return -1
         return ans + min_index
-
-        
-
-
