fe232e02529ce3dc800a58863fb5ac28c3686a5e
quanjingchen <quanjingchen@gmail.com>
2023-03-30 17:12:47 -0700

    Time: 468 ms (35.14%), Space: 28.7 MB (11.11%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        mySet = set()
+        for num in nums:
+            if num in mySet:
+                return True
+            else:
+                mySet.add(num)
+        return False
