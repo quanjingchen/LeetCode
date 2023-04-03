7553cb9eae6870278097e16fb6964ef2a4aa95c7
quanjingchen <quanjingchen@gmail.com>
2023-04-03 15:12:35 -0700

    Time: 174 ms (94.31%), Space: 20.9 MB (80.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,17 +7,16 @@ class Solution(object):
         length = len(nums)
         prefix = [0] * length
         prefix[0] = 1
-        sufix = [0] * length
-        sufix[length - 1] = 1
+
         for i in range(1, length):
             prefix[i] = prefix[i - 1] * nums[i - 1]
-            
-        for i in reversed(range(len(nums) - 1)):
-            sufix[i] = sufix[i + 1] * nums[i + 1]
         
-        ans = []
-        for i in range(len(nums)):
-            ans.append(prefix[i] * sufix[i])
-        return ans
+        sufix = 1
+        for i in reversed(range(len(nums) - 1)):
+            sufix = sufix * nums[i + 1]
+            prefix[i] = prefix[i] * sufix
+        
+
+        return prefix
             
         
