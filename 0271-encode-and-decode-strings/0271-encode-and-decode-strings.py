5d2ba8fbbfbf030b70dd17cbcf9d834f84987b5f
quanjingchen <quanjingchen@gmail.com>
2023-04-04 10:37:46 -0700

    Time: 48 ms (82.75%), Space: 13.9 MB (90.98%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -6,12 +6,7 @@ class Codec:
         :type strs: List[str]
         :rtype: str
         """
-        res = []
-        for s in strs:
-            res.append(str(len(s)))
-            res.append('/')
-            res.append(s)
-        return ''.join(res)
+        return unichr(257).join(strs)
 
     def decode(self, s):
         """Decodes a single string to a list of strings.
@@ -19,10 +14,4 @@ class Codec:
         :type s: str
         :rtype: List[str]
         """
-        res, i = [], 0
-        while i < len(s):
-            idx = s[i:].find('/')
-            size = int(s[i:i+idx])
-            res.append(s[i+idx+1:i+idx+size+1])
-            i += idx+size+1
-        return res
+        return s.split(unichr(257))
