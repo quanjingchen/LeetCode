fe45989449d1d3dfd8baaf102811414344375654
quanjingchen <quanjingchen@gmail.com>
2023-04-18 18:36:08 -0700

    Time: 141 ms (13.46%), Space: 15.2 MB (9.60%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,17 +1,17 @@
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isIdentical(node1: Optional[TreeNode], node2: Optional[TreeNode]) -> bool:
+            if node1 is None or node2 is None:
+                return node1 is None and node2 is None
+
+            return (node1.val == node2.val and
+                    isIdentical(node1.left, node2.left) and
+                    isIdentical(node1.right, node2.right))
+
         if root is None:
             return False
 
-        if self.isIdentical(root, subRoot):
+        if isIdentical(root, subRoot):
             return True
 
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-
-    def isIdentical(self, node1: Optional[TreeNode], node2: Optional[TreeNode]) -> bool:
-        if node1 is None or node2 is None:
-            return node1 is None and node2 is None
-
-        return (node1.val == node2.val and
-                self.isIdentical(node1.left, node2.left) and
-                self.isIdentical(node1.right, node2.right))
