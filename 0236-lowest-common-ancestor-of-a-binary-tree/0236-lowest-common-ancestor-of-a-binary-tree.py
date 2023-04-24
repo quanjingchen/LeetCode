3fb4eff81eafaadbe4f9243e2d9306a3a12f5d7e
quanjingchen <quanjingchen@gmail.com>
2023-04-24 15:28:25 -0700

    Time: 75 ms (44.36%), Space: 26.2 MB (80.29%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -13,27 +13,12 @@ class Solution:
         :type q: TreeNode
         :rtype: TreeNode
         """
-        def recurse_tree(current_node):
-            # If reached the end of a branch, return None.
-            if not current_node:
-                return None
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
 
-            # Left Recursion
-            left = recurse_tree(current_node.left)
 
-            # Right Recursion
-            right = recurse_tree(current_node.right)
-
-            # If the current node is one of p or q
-            if current_node == p or current_node == q:
-                return current_node
-
-            # If both left and right are not None, it means we found both p and q in the subtree rooted at the current node.
-            if left and right:
-                return current_node
-
-            # Return the non-None value if either left or right is not None, else return None.
-            return left if left else right
-
-        # Traverse the tree and return the result
-        return recurse_tree(root)
