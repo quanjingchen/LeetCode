ed2bf2156df07d7115f9fa0f6dc08ed80c3388a9
quanjingchen <quanjingchen@gmail.com>
2023-04-26 21:25:17 -0700

    Time: 9351 ms (7.64%), Space: 18.6 MB (26.91%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = word
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+
+        def dfs(r, c, node):
+            char = board[r][c]
+            if char not in node.children:
+                return
+
+            child = node.children[char]
+            if child.word is not None:
+                paths.append(child.word)
+                child.word = None  # Remove the word to prevent duplicates
+
+            board[r][c] = '#'  # Mark the cell as visited
+            for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                if 0 <= nr < rows and 0 <= nc < cols:
+                    dfs(nr, nc, child)
+            board[r][c] = char  # Restore the cell
+
+        paths = []
+        rows, cols = len(board), len(board[0])
+
+        trie = Trie()
+        for word in words:
+            trie.add(word)
+
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, trie.root)
+
+        return paths
