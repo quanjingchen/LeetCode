880642c440d64036a3e76cd821ed744ee799d4f1
quanjingchen <quanjingchen@gmail.com>
2023-04-26 21:47:12 -0700

    Time: 9947 ms (5.02%), Space: 19.3 MB (5.39%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,10 +1,12 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.word = None
+        self.word = ''
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+
     def add(self, word):
         node = self.root
         for char in word:
@@ -12,51 +14,42 @@ class Trie:
                 node.children[char] = TrieNode()
             node = node.children[char]
         node.word = word
-        
+
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-
         
         def dfs(r, c, node):
+            if node.word:
+                paths.add(node.word)
+            if not node.children:
+                return
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return
 
-            char = board[r][c]
-            
-            if char not in node.children:
+            if board[r][c] not in node.children:
                 return
             
-            child = node.children[char]
-            
-            if child.word:
-                paths.append(child.word)
-                child.word = None
-                
-            # mark the cell
+            char = board[r][c]
             board[r][c] = '#'
-            # check the neighbors
-            for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                if 0 <= nr < rows and 0 <= nc < cols:
-                    dfs(nr, nc, child)
-            # clean up
+            for neighbor in neighbors:
+                new_r = r + neighbor[0]
+                new_c = c + neighbor[1]
+                dfs(new_r, new_c, node.children[char])
             board[r][c] = char
-        
-        
-        paths = []
+
+        paths = set()
         rows = len(board)
         cols = len(board[0])
         visited = [[False for _ in range(cols)] for _ in range(rows)]
-        neighbours = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        # build the Trie:
+        neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
         trie = Trie()
         for word in words:
             trie.add(word)
         node = trie.root
-        # run the dfs    
+
         for r in range(rows):
             for c in range(cols):
                 dfs(r, c, node)
-                
-        return paths
-            
-            
-            
-        
+
+        return list(paths)
